a3ff042e12420c3e0fa610490667bc5f8813f657
Valentin Tsonev <32265202+valkin88@users.noreply.github.com>
2017-12-22 11:15:09 +0200

    Add files via upload
    Uploading First Steps in Coding

---
+++ Programming Basics - First Steps in Coding/RectangleArea/RectangleArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STriangle1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine(a * b);
+        }
+    }
+}
---
+++ Programming Basics - First Steps in Coding/SquareOfStars/SquareOfStars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("*");
+                for (int j = 1; j < n; j++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
